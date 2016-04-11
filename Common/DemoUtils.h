4e0948210a1add26308a8ac61879b4a4a77e6889
Dustin Biser <dustinbiser@gmail.com>
2016-04-11 17:23:46 -0400

    Added tinyobjloader to Extern

--- Common/DemoUtils.h
+++ Common/DemoUtils.h
@@ -33,47 +33,6 @@ inline void GetAssetsPath(_Out_writes_(pathSize) WCHAR* path, UINT pathSize)
 	}
 }
 
-inline HRESULT ReadDataFromFile(LPCWSTR filename, byte** data, UINT* size)
-{
-	using namespace Microsoft::WRL;
-
-	CREATEFILE2_EXTENDED_PARAMETERS extendedParams = {};
-	extendedParams.dwSize = sizeof(CREATEFILE2_EXTENDED_PARAMETERS);
-	extendedParams.dwFileAttributes = FILE_ATTRIBUTE_NORMAL;
-	extendedParams.dwFileFlags = FILE_FLAG_SEQUENTIAL_SCAN;
-	extendedParams.dwSecurityQosFlags = SECURITY_ANONYMOUS;
-	extendedParams.lpSecurityAttributes = nullptr;
-	extendedParams.hTemplateFile = nullptr;
-
-	Wrappers::FileHandle file(CreateFile2(filename, GENERIC_READ, FILE_SHARE_READ, OPEN_EXISTING, &extendedParams));
-	if (file.Get() == INVALID_HANDLE_VALUE)
-	{
-		throw new std::exception();
-	}
-
-	FILE_STANDARD_INFO fileInfo = {};
-	if (!GetFileInformationByHandleEx(file.Get(), FileStandardInfo, &fileInfo, sizeof(fileInfo)))
-	{
-		throw new std::exception();
-	}
-
-	if (fileInfo.EndOfFile.HighPart != 0)
-	{
-		throw new std::exception();
-	}
-
-	*data = reinterpret_cast<byte*>(malloc(fileInfo.EndOfFile.LowPart));
-	*size = fileInfo.EndOfFile.LowPart;
-
-	if (!ReadFile(file.Get(), *data, fileInfo.EndOfFile.LowPart, nullptr, nullptr))
-	{
-		throw new std::exception();
-	}
-
-	return S_OK;
-}
-
-
 #if defined(_DEBUG)
     // Assign a name to the object to aid with debugging.
     inline void SetName(ID3D12Object* pObject, LPCWSTR name)
